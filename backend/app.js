086dfeb2c6dcb4b1d4c4f8f8ae6d3a3df011d1a1
Tom CWA <tom@agence-cwa.fr>
2021-05-03 18:12:09 +0200

    Installation du projet backend + connexion MySQL

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+// Importer les packages
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+// Headers pour contourner les erreurs de CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.status(201);
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
+  });
+  
+  app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
+  });
+  
+
+// Rendre la requête exploitable
+app.use(express.json());
+
+module.exports = app;
