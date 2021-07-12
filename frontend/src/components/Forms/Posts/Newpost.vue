18cb077d301ce554330dc1242fc4c78bf6590aa8
Tom Texier <tom.texierpro@gmail.com>
2021-07-12 09:44:52 +0200

    Search Gif

--- frontend/src/components/Forms/Posts/Newpost.vue
+++ frontend/src/components/Forms/Posts/Newpost.vue
@@ -1,8 +1,15 @@
 <template>
     <form>
         <div class="input-gif">
-            <input type="text" placeholder="Rechercher un gif..." @focus="inputFocused" @blur="inputFocused">
-            <div v-if="reveal" class="datalist"></div>
+            <input v-model="query" type="text" placeholder="Rechercher un gif..." @click="inputFocused" @keyup="searchGif">
+            <div v-if="reveal" class="datalist">
+                <div class="results" v-if="results && results.length != 0">
+                    <img :src="gif.images.fixed_width_small.url" class="image-card" v-for="gif in results" :key="gif.id" @click="selectGif">
+                </div>
+                <div class="empty" v-if="results.length == 0 || !results">
+                    Aucun résultat
+                </div>
+            </div>
         </div>
         <p class="alternative">Ou insérer une image</p>
         <textarea placeholder="Ajouter une description"></textarea>
@@ -11,16 +18,44 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: "FormNewpost",
     data() {
         return {
-            reveal: false
+            reveal: false,
+            results: false,
+            query: "",
+            apiKey: "05pHQKk692kM4shQ4Bh7Ta7n4CuhgW2j",
+            apiUrl: "https://api.giphy.com/v1/gifs/search",
+            gifSelected: ""
         }
     },
     methods: {
         inputFocused() {
-            this.reveal = !this.reveal;
+            this.reveal = true;
+        },
+        searchGif() {
+            axios.get(this.apiUrl, {
+                params: {
+                    api_key: this.apiKey,
+                    q: this.query.split(' ').join('+'),
+                    limit: 25,
+                    lang: 'fr'
+                }
+            })
+            .then(response => {
+                this.results = response.data.data;
+            })
+            .catch(error => {
+                console.log("Erreur : ", error);
+            })
+        },
+        selectGif(e) {
+            this.gifSelected = e.currentTarget.getAttribute('src');
+            this.reveal = false;
+            console.log(this.gifSelected);
         }
     }
 }
